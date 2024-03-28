4aa8079dab3e4676ba6b72c2729fcf470c08dbcb
Pratik Kadam <pratiksuhaskadam@gmail.com>
2024-03-28 20:14:12 +0530

    bike program

---
+++ Practice/Bike.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+public class Bike {
+	//Variable
+	String Colour,Brand;
+	//Constructor
+	public Bike() {
+		Colour="Black";Brand="RoyalEnfild";
+	}
+	//Method
+	public void State1() {
+		System.out.println("Switch On");
+	}
+	public void State2() {
+		System.out.println("Switch Off");
+	}
+	public void State3() {
+		System.out.println("Accelarate");
+	}
+}
