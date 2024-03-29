e9fd932d9e668f24b6fcb694ebc2778e818fb77b
Ashish Bagane <abagane32@gmail.com>
2024-03-29 10:43:14 -0700

    Local Vaiables practice

--- Ashish/OOP/src/com/examples/variable/Program.java
+++ Ashish/OOP/src/com/examples/variable/Program.java
@@ -7,6 +7,18 @@ public class Program {
 		var a = 10;
 		
 		System.out.println(a);
+		
+		// Statick variable do not required instance of Class
+		System.out.println(Student.staticToClass);
+		
+		Student student = new Student();
+		System.out.println(student.staticToClass);
+		System.out.println(student.localToClass);
+		student.Display();
+		System.out.println(student.getValue());
+		
+
+
 	}
 
 }
