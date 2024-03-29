748bb1418581081badf8c745692f13a55fab149d
Ashish Bagane <abagane32@gmail.com>
2024-03-29 11:12:18 -0700

    Interfaces

--- Ashish/OOP/src/com/examples/variable/Program.java
+++ Ashish/OOP/src/com/examples/variable/Program.java
@@ -6,7 +6,24 @@ public class Program {
 		
 		var a = 10;
 		
-		System.out.println(a);
+		//System.out.println(a);
+		
+		Bike bike = new Bike();
+		bike.WhoIAm();
+		bike.GetStart();
+		System.out.println(Bike.Color);
+		
+		bike.SetColor();
+		System.out.println(Bike.Color);
+		
+//		IEngine pulsar = new Bike();
+//		pulsar.SetColor(); 
+//		System.out.println(Bike.Color);
+//		System.out.print("Pulsar : ");
+//		
+//		pulsar.GetStart();
+//		 
+	
 		
 		// Statick variable do not required instance of Class
 		System.out.println(Student.staticToClass);
