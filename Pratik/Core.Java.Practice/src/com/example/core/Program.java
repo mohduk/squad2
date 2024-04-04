809856b800aa64a4df9dd3bd597c1c55f85dcbe1
Pratik Kadam <pratiksuhaskadam@gmail.com>
2024-04-04 11:26:28 +0530

    Fibonacci Completed

--- Pratik/Core.Java.Practice/src/com/example/core/Program.java
+++ Pratik/Core.Java.Practice/src/com/example/core/Program.java
@@ -5,6 +5,12 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		// Fibonacci Serise		
+		//FibonacciSeries fibonacciSeries = new FibonacciSeries();
+		//fibonacciSeries.getFibonacciNumber();
+		
+		// Prime
 	}
 
 }
