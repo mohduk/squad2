d7b53e027764e3b6ffa54456ed87623389a317b8
Pratik Kadam <pratiksuhaskadam@gmail.com>
2024-04-24 12:31:58 +0530

    SQL

---
+++ Pratik/SQL/PRACTICE/SQLQuery2.sql
@@ -0,0 +1,44 @@
+
+
+
+CREATE TABLE deparment
+(
+id int identity(1,2),
+name varchar(50)
+primary key (id)
+);
+
+ALTER TABLE student
+add marks int ,
+add departmentid int
+
+
+
+select * from student
+
+where id = 1
+
+
+insert into student values ('Shailendra', 1, 20, 1)
+
+
+insert into deparment values ('JAVA')
+
+insert into deparment values ('NET')
+
+
+insert into student values ( 'Pratik', 34)
+
+insert into student values (null)
+insert into student values (null, null)
+
+
+
+select * from department
+select * from student
+
+select s.name, d.name from student s
+join department d
+on s.departmentid = d.id 
+
+
